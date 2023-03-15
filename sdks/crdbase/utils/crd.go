d4da30a63db8a7f5a858bf3b1f093818e134baca
zzjin <tczzjin@gmail.com>
2023-03-15 23:12:48 +0800

    Update tests
    Signed-off-by: zzjin <tczzjin@gmail.com>

--- sdks/crdbase/utils/crd.go
+++ sdks/crdbase/utils/crd.go
@@ -21,6 +21,46 @@ import (
 	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 )
 
+var (
+	_baseSchemaProps = map[string]apiextv1.JSONSchemaProps{
+		"apiVersion": {
+			Description: `APIVersion defines the versioned schema of this representation of an object.
+Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values.
+More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources`,
+			Type: "string",
+		},
+		"kind": {
+			Description: `Kind is a string value representing the REST resource this object represents.
+Servers may infer this from the endpoint the client submits requests to.
+Cannot be updated. In CamelCase.
+More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds`,
+			Type: "string",
+		},
+		"metadata": {
+			Type: "object",
+		},
+		"spec": {
+			Description: "crd spec defines the real saved data",
+			Type:        "object",
+		},
+		"status": {
+			Description: "status defines the observed state of saved data",
+			Type:        "object",
+		},
+	}
+)
+
+// NewCRDJSONSchemaProps use deep-copy to new object, cannot change _baseSchemaProps
+func NewCRDJSONSchemaProps() map[string]apiextv1.JSONSchemaProps {
+	return map[string]apiextv1.JSONSchemaProps{
+		"apiVersion": {Description: _baseSchemaProps["apiVersion"].Description, Type: _baseSchemaProps["apiVersion"].Type},
+		"kind":       {Description: _baseSchemaProps["kind"].Description, Type: _baseSchemaProps["kind"].Type},
+		"metadata":   {Type: _baseSchemaProps["metadata"].Type},
+		"spec":       {Description: _baseSchemaProps["spec"].Description, Type: _baseSchemaProps["spec"].Type},
+		"status":     {Description: _baseSchemaProps["status"].Description, Type: _baseSchemaProps["status"].Type},
+	}
+}
+
 func Name2KindNames(name string) apiextv1.CustomResourceDefinitionNames {
 	kebab := strcase.ToKebab(name)
 
